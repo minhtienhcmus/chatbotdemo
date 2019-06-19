057d3822b0ba317617f4cd1b355e80436f5f10e1
minhtienhcmus <minhtienhcmus@gmail.com>
2019-06-19 14:36:01 +0700

    new

---
+++ ChatBot_Api.py
@@ -0,0 +1,25 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort,url_for,send_from_directory,make_response
+import json
+from googletrans import Translator
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("new_index.html")
+
+@app.route('/translate',methods=['GET', 'POST'])
+def translate():
+    text=request.data.decode('utf-8')
+    translator = Translator()
+    print("translating ...")
+    translations = translator.translate([text], dest='vi')
+    for translation in translations:
+        print(translation.origin, ' -> ', translation.text)
+    string_me=text
+    string_bot=translation.text
+    print("done")
+    return json.dumps({"me":string_me,"bot":string_bot})
+ 
+if __name__ == '__main__':
+	app.run(port="8889",debug=True)
